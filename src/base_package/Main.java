c1a3e1d91dbe965f62bf98f65037bd6f6a5fdc4c
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-13 21:35:21 -0600

    primitive types

--- src/base_package/Main.java
+++ src/base_package/Main.java
@@ -2,6 +2,12 @@ package base_package;
 
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello World!");
+        byte myAge = 27;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter ='A';
+        boolean isEligible = false;
+
+        System.out.println(herAge);
     }
 }
