fef5249717a107a2df0b7b3a198a78224a845c84
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-16 14:32:39 -0600

    email application start

---
+++ src/emailapp/Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+//constructor receive first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    //ask for department
+    //generate random password
+    //set mailbox capacity
+    //set the alternate email
+    //change the password
+
+}
