679303a8492d3d70d1b2f7991eb5d9cd6d748ea6
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-18 23:11:41 -0600

    Set student id

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,27 +6,34 @@ public class Student {
     private String firstName;
     private String lastName;
     private int academicYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private int courseCost = 600;
+    private static int courseCost = 600;
+    private static int id = 1000;
 
     //constructor prompt: name and year
-public Student(){
-    Scanner in = new Scanner(System.in);
-    System.out.println("Please enter student first name: ");
-    this.firstName = in.nextLine();
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter student first name: ");
+        this.firstName = in.nextLine();
 
-    System.out.println("Please enter student last name: ");
-    this.lastName = in.nextLine();
+        System.out.println("Please enter student last name: ");
+        this.lastName = in.nextLine();
 
-    System.out.println("Please select student academic year.\n1. Freshmen\n2. Sophomore\n3. Junior\n4. Senior");
-    this.academicYear = in.nextInt();
+        System.out.println("Please select student academic year.\n1. Freshmen\n2. Sophomore\n3. Junior\n4. Senior");
+        this.academicYear = in.nextInt();
 
-    System.out.println(firstName +" "+lastName +" "+academicYear);
-}
+        System.out.println(firstName + " " + lastName + " " + academicYear);
+        setStudentID();
+    }
+
     //generate student id
-
+    private void setStudentID() {
+        //grade level + ID
+        id++;
+        studentID = academicYear + "" + id;
+    }
     //enroll in courses
 
     //view balance
