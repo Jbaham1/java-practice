ee439a87fe6b06fec92d6a27b98052b79fd460fe
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-18 23:06:27 -0600

    student constructor

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,37 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int academicYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int courseCost = 600;
+
+    //constructor prompt: name and year
+public Student(){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please enter student first name: ");
+    this.firstName = in.nextLine();
+
+    System.out.println("Please enter student last name: ");
+    this.lastName = in.nextLine();
+
+    System.out.println("Please select student academic year.\n1. Freshmen\n2. Sophomore\n3. Junior\n4. Senior");
+    this.academicYear = in.nextInt();
+
+    System.out.println(firstName +" "+lastName +" "+academicYear);
+}
+    //generate student id
+
+    //enroll in courses
+
+    //view balance
+
+    // pay tuition
+
+    //show status
 }
--- src/studentdatabaseapp/StudentDataBaseApp.java
+++ src/studentdatabaseapp/StudentDataBaseApp.java
@@ -1,4 +1,10 @@
 package studentdatabaseapp;
 
 public class StudentDataBaseApp {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        //ask how many new users we want to add
+
+        // create n number of new students
+    }
 }
